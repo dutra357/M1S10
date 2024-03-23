e89bdc9afab1d19f9bebe77fe5d43c826d893320
David Dutra <dadutra@hotmail.com>
2024-03-22 21:24:15 -0300

    Início do exercício M1S20 - Ex. 01

---
+++ src/main/java/com/br/FullStack/M1S10/entities/MaterialEntity.java
@@ -0,0 +1,24 @@
+package com.br.FullStack.M1S10.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "materiais")
+public class MaterialEntity {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "agenda_id", nullable = false)
+    private AgendaEntity agenda;
+    
+
+    @Column(nullable = false)
+    private String caminhoArquivo;
+}
